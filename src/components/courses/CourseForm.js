d010cc913aa8c60437a852c8e6598fce9a1d50ed
AhmedElselly <ahmedelselly87@gmail.com>
2021-04-15 14:30:44 +0200

    imported redirect from react-router-dom

--- src/components/courses/CourseForm.js
+++ src/components/courses/CourseForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Redirect} from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
